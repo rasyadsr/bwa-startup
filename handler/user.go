2f19418991a07b773875edfbb8c127ea06a7e4dd
Mochamad Rasyad <m.rasyad.cr7@gmail.com>
2023-02-26 19:11:01 +0700

    feat(user): init projecr & register user endpoint

---
+++ handler/user.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"bwastartup/helper"
+	"bwastartup/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(serivce user.Service) *userHandler {
+	return &userHandler{serivce}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"erros": errors}
+
+		response := helper.ApiResponse("Register account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+
+	if err != nil {
+		response := helper.ApiResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, "tokentoken")
+
+	response := helper.ApiResponse("Account has been registered", http.StatusCreated, "success", formatter)
+
+	c.JSON(http.StatusCreated, response)
+}
